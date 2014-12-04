046e6814c181db2862511f5fa773527eea7b389b
biggers <biggers@utsl.com>
2014-12-04 16:08:21 -0500

    playbooks/nova-controller.yaml: new task 'add devel_packages' Vagrantfile: "bump up" memory on all nodes vms/run-sample-session.sh: minor changes

--- vms/run-sample-session.sh
+++ vms/run-sample-session.sh
@@ -3,12 +3,15 @@
 set -x
 set -e
 
+# source quantum-ansible.rc
 . /vagrant/openrc
 
-nova flavor-create micro 6 50 0 1 || true
+nova flavor-create micro 6 50 0 1 || echo "failed"
 
-chmod 0600 /vagrant/test-key
-nova keypair-add --pub-key /vagrant/test-key.pub test-key
+TEST_KEY=/vagrant/test-key
+# TEST_KEY=./test-key
+chmod 0600 ${TEST_KEY}
+nova keypair-add --pub-key ${TEST_KEY}.pub test-key
 
 neutron net-create net1
 neutron subnet-create net1 10.0.33.0/24 --name=sub1 --dns_nameservers 8.8.4.4 8.8.8.8
@@ -33,15 +36,19 @@ PORT_ID=$(neutron port-create --security-group $TEST_SG net1 \
 
 sleep 5 # TODO find out why dhcp replies are dropped w/o this delay
 
-nova boot --flavor micro --image cirros-0.3.1-x86_64 --nic port-id=$PORT_ID \
-          --key-name test-key vm1
+VM=vm_tng
+nova boot --flavor micro --image cirros-0.3.1-x86_64 --nic port-id=$PORT_ID --key-name test-key --security-group $TEST_SG   ${VM}
 
 
-FLOAT_ID=`neutron floatingip-create ext-net | grep ' id ' | awk '{print $4;}'`
-VM_ID=`nova list | grep vm1 | awk '{print $2;}'`
+FLOAT_ID=$(neutron floatingip-create ext-net | grep ' id ' | awk '{print $4;}')
+VM_ID=$(nova list | grep ${VM} | awk '{print $2;}')
+
 
 neutron floatingip-associate $FLOAT_ID $PORT_ID
 neutron floatingip-show $FLOAT_ID
 
 eval `neutron floatingip-show -f shell --variable floating_ip_address $FLOAT_ID`
 echo "Give VM a minute to boot before trying ssh -i vms/test-key cirros@${floating_ip_address} from main host"
+
+
+
